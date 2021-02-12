e3c6cc5fb75e18a50422155aa3633198fddb5a7f
Eduardo Ramirez <jose.ralarcon@alumnos.udg.mx>
2021-02-12 16:57:10 -0600

    Menu animation corrections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 selector('.menu').addEventListener('click', function(){
   // this.classList.toggle('open');
+  // document.querySelector("nav").style.display = "flex";
+  // document.querySelector("nav").style.transform = "rotateY(180deg)";
   selector('header').classList.toggle('open');
   selector('.overlay').classList.toggle('open');
 });
@@ -12,3 +14,21 @@ selector('.overlay').addEventListener('click', function(){
 function selector(s){
   return document.querySelector(s);
 }
+
+window.onload = function(){
+  document.querySelector('body').classList.remove('preload');
+}
+
+let resizeTimer;
+window.addEventListener("resize", () => {
+  var width = window.innerWidth;
+  console.log(width);
+  if(width <= 768){
+    document.body.classList.add("resize-animation-stopper");
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+      document.body.classList.remove("resize-animation-stopper");
+    }, 400);
+  }
+});
+
