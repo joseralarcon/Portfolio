10dc84eb45f04bc383e70626e979295afda7588e
Eduardo Ramirez <jose.ralarcon@alumnos.udg.mx>
2021-02-12 16:04:38 -0600

    Menu and styles modified

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+selector('.menu').addEventListener('click', function(){
+  // this.classList.toggle('open');
+  selector('header').classList.toggle('open');
+  selector('.overlay').classList.toggle('open');
+});
+
+selector('.overlay').addEventListener('click', function(){
+  selector('header').classList.toggle('open');
+  selector('.overlay').classList.toggle('open');
+});
+
+function selector(s){
+  return document.querySelector(s);
+}
