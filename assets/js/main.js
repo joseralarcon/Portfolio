f375e9f8a6c1a7dca2f82bd7ec2e5c548f7f3dfd
Eduardo Ramirez <jose.ralarcon@alumnos.udg.mx>
2021-02-17 20:47:22 -0600

    Home 1.2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
-// let header = selector('nav');
+let header = selector('nav');
 
-// window.addEventListener('scroll', function(){
-//   if (!window.matchMedia("(max-width: 768px)").matches) {
-//     /* The viewport is not less than, or equal to, 768 pixels wide */
-//     header.classList.toggle('sticky', window.scrollY>0);
-//   }
-// });
+window.addEventListener('scroll', function(){
+  if (!window.matchMedia("(max-width: 768px)").matches) {
+    /* The viewport is not less than, or equal to, 768 pixels wide */
+    header.classList.toggle('sticky', window.scrollY>0);
+  }
+});
 
 const mediaQueryList = window.matchMedia('(max-width: 768px)');
 const headerLinks = document.querySelectorAll('.header__a');
@@ -18,10 +18,21 @@ if(mediaQueryList.matches) {
 mediaQueryList.addEventListener('change', event => {
   if (event.matches) {
     //Add event listener. Window is less than or equal to 768px on change.
+    let resizeTimer;
+    document.body.classList.add("resize-animation-stopper");
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+      document.body.classList.remove("resize-animation-stopper");
+    }, 400);
+
     addEventLinks();
+    header.classList.remove('sticky');
   }else{
     //Remove event listener. Window is above than or equal to 768px on change.;
     removeEventLinks();
+    if(window.scrollY>0){
+      header.classList.toggle('sticky')
+    }
   }
 })
 
@@ -54,17 +65,25 @@ window.onload = function(){
   selector('body').classList.remove('preload');
 }
 
-let resizeTimer;
-let bandera = false;
-window.addEventListener("resize", () => {
-  let width = window.innerWidth;
-  if(width <= 768){
-    bandera = true;
-    // header.classList.remove('sticky');
-    document.body.classList.add("resize-animation-stopper");
-    clearTimeout(resizeTimer);
-    resizeTimer = setTimeout(() => {
-      document.body.classList.remove("resize-animation-stopper");
-    }, 400);
-  }
-});
+/*About Section tabs*/
+function sectionTabs(){
+  const aboutSection = selector('.about__section');
+  const tabsContainer = document.querySelectorAll('.about__tab-item');
+
+  tabsContainer.forEach(tab => {
+    tab.addEventListener('click', (e) => {
+      if(!e.target.classList.contains('active')){
+        const target = e.target.getAttribute('data-target');
+        const removeActive = document.querySelector('.about__tab-item.active');
+        removeActive.classList.remove('active');
+        e.target.classList.add('active');
+
+        aboutSection.querySelector('.tab-content.active').classList.remove('active');
+        aboutSection.querySelector(target).classList.add('active');
+      }
+    });
+  });
+
+}
+
+sectionTabs();
